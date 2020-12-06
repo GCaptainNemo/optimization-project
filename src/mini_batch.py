fa4d941e06070120be17077f2e1265ac2024052f
why <1136003685@qq.com>
2020-12-06 14:30:25 +0800

    finish three optimization method

---
+++ src/mini_batch.py
@@ -0,0 +1,107 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from sklearn.datasets import fetch_rcv1
+import numpy as np
+import torch.utils.data as Data
+import pickle
+
+# 超参数
+BATCH_SIZE = 32
+EPOCH = 1000
+
+# 加载数据
+class DataAccessObject:
+    def __init__(self):
+        """
+            dir(rcv1) = ['DESCR', 'data', 'sample_id', 'target', 'target_names']
+            data是 n_samples x n_feature的二维numpy.ndarray数组
+            target是 n_samples一维numpy.ndarray数组
+        """
+        self.load_data()
+        self.load_batch()
+
+    def load_data(self):
+        rcv1 = fetch_rcv1(subset='train', download_if_missing=False)
+        x = rcv1.data.A  # numpy.float64
+        x = x.astype(np.float32)  # 修改数据类型，否则就会出错
+        self.xArray = torch.from_numpy(x)
+        # csr_matrix -> numpy.ndarray -> torch.tensor
+        y = rcv1.target.A
+        y = y.astype(np.float32)  # 修改数据类型，否则就会出错
+        self.yArray = torch.from_numpy(y)
+
+    def load_batch(self):
+        self.torch_dataset = Data.TensorDataset(self.xArray, self.yArray)
+        self.loader = Data.DataLoader(
+            dataset=self.torch_dataset,
+            batch_size=BATCH_SIZE,
+            shuffle=True,  # true表示每个epoch需要洗牌
+            num_workers=2,  # 每次训练有两个线程进行的
+        )
+
+
+# 定义LogisticRegression
+class LogisticRegression(nn.Module):
+    def __init__(self):
+        super(LogisticRegression, self).__init__()
+        # linearTransform = wx + b
+        self.linearTransform = nn.Linear(47236, 103)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.linearTransform(x)
+        x = self.sigmoid(x)
+        return x
+
+
+class MyLossFunction(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, y):
+        return torch.mean(-y * torch.log(x) + - (1 - y) * torch.log(1 - x))
+
+
+# optimization process
+if __name__ == '__main__':
+    logistic_model = LogisticRegression()
+    DAO = DataAccessObject()
+    criterion = MyLossFunction()
+    # criterion = nn.BCELoss()
+    use_gpu = torch.cuda.is_available()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    logistic_model.to(device)
+    criterion.to(device)
+
+    # if(use_gpu):
+    #     logistic_model = logistic_model.cuda()
+    #     criterion = criterion.cuda()
+    #     xArray = xArray.cuda()
+    #     yArray = yArray.cuda()
+    optimizer = torch.optim.SGD(logistic_model.parameters(),
+                                lr=1e-3)
+    loss_lst = []
+    for num_epoch in range(1000):
+        print(num_epoch)
+        for step, (batch_x, batch_y) in enumerate(DAO.loader):
+            # b_x = Variable(batch_x)
+            # b_y = Variable(batch_x)
+            batch_x = batch_x.to(device)
+            batch_y = batch_y.to(device)
+
+            output = logistic_model(batch_x)  # get_out for every net
+            loss = criterion(output, batch_y)  # compute loss for every net
+            # if don't call zero_grad,
+            # the grad of each batch will be accumulated
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step() # apply gradient
+            loss_lst.append(loss.item()) # loss recoder
+        # if (num_epoch + 1) % 5 == 0:
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(num_epoch + 1, EPOCH, loss.item()))
+    torch.save(logistic_model, "mini_batchGD_model_epoch1000.pt")
+    with open("loss_minibatch.pkl", "wb") as f:
+        pickle.dump(loss_lst, f)
+    print(loss)
+
--- src/test.py
+++ src/test.py
@@ -1,13 +1,49 @@
 import numpy as np
 import torch
-from src.main import LogisticRegression
+from sklearn.datasets import fetch_rcv1
+from src.SGD import LogisticRegression
 
 # new = LogisticRegression()
 
-#
-the_model = torch.load("BGD_model.pt")
+class DataAccessObject:
+    def __init__(self):
+        """
+            dir(rcv1) = ['DESCR', 'data', 'sample_id', 'target', 'target_names']
+            data是 n_samples x n_feature的二维numpy.ndarray数组
+            target是 n_samples一维numpy.ndarray数组
+        """
+        self.load_data()
+
+    def load_data(self):
+        rcv1 = fetch_rcv1(subset='train', download_if_missing=False)
+        x = rcv1.data.A  # numpy.float64
+        x = x.astype(np.float32)  # 修改数据类型，否则就会出错
+        self.xArray = torch.from_numpy(x)
+        # csr_matrix -> numpy.ndarray -> torch.tensor
+        y = rcv1.target.A
+        y = y.astype(np.float32)  # 修改数据类型，否则就会出错
+        self.yArray = torch.from_numpy(y)
+
+
+the_model = torch.load("mini_batchGD_model_epoch1000.pt")
+the_model.to(torch.device("cpu"))
 print(the_model.linearTransform.weight[0])
 print(type(the_model))
+
+
+if __name__ == "__main__":
+    DAO = DataAccessObject()
+
+    # for i in range(DAO.xArray.shape[0]):
+    output = the_model(DAO.xArray)
+    mask = output.ge(0.5).float()
+    correct = (mask == DAO.yArray).sum()
+    acc = correct.item() / DAO.xArray.size(0)
+
+    print("acc = ", acc)
+    print(DAO.xArray.size(0))
+
+
 
 
 
