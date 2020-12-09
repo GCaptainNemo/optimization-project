89254ce770056722907a455236790a75f04edc0d
why <1136003685@qq.com>
2020-12-09 18:57:49 +0800

    finish homework

--- src/SGD.py
+++ src/SGD.py
@@ -38,7 +38,7 @@ class DataAccessObject:
         self.torch_dataset = Data.TensorDataset(self.xArray, self.yArray)
         self.loader = Data.DataLoader(
             dataset=self.torch_dataset,
-            batch_size=BATCH_SIZE,
+            batch_size=1000,
             shuffle=True,  # true表示每个epoch需要洗牌
             num_workers=2,  # 每次训练有两个线程进行的
         )
@@ -96,44 +96,61 @@ if __name__ == '__main__':
     DAO = DataAccessObject()
     criterion = MyLossFunction()
     # criterion = nn.BCELoss()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
     logistic_model.to(device)
     criterion.to(device)
     optimizer = torch.optim.SGD(logistic_model.parameters(),
                                 lr=1e-3)
     loss_lst = []
-    for num_epoch in range(EPOCH):
+    per_iter_loss_lst = []
+    # CPU = torch.device("cpu")
+    DAO.xArray.to(device)
+    DAO.yArray.to(device)
+
+    for num_epoch in range(2):
         print(num_epoch)
-
-
-        prefetcher = data_prefetcher(DAO.loader)
-        batch_x, batch_y = prefetcher.next()
-        i = 0
-        while batch_x is not None:
-            print(i, len(batch_x))
-            i += 1
-            data = prefetcher.next()
-
-        # for step, (batch_x, batch_y) in enumerate(DAO.loader):
+        # prefetcher = data_prefetcher(DAO.loader)
+        # data = prefetcher.next()
+        # # i = 0
+        # while data is not None:
+        #     # print(i, len(batch_x))
+        #     # i += 1
+        #     data = prefetcher.next()
+        #     batch_x, batch_y = data
+        for step, (batch_x, batch_y) in enumerate(DAO.loader):
             batch_x = batch_x.to(device)
             batch_y = batch_y.to(device)
+            for i in range(len(batch_x)):
+                output = logistic_model(batch_x[i])  # get_out for every net
+                classify_loss = criterion(output, batch_y[i])  # compute loss for every net
+                regular_loss = 0
+                for par in logistic_model.parameters():
+                    regular_loss += torch.sum(torch.pow(par, 2))
+                loss = classify_loss + LAMBDA * regular_loss
 
-            output = logistic_model(batch_x)  # get_out for every net
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step() # apply gradient
+                loss_lst.append(loss.item()) # loss recoder
 
-            classify_loss = criterion(output, batch_y)  # compute loss for every net
-            regular_loss = 0
-            for par in logistic_model.parameters():
-                regular_loss += torch.sum(torch.pow(par, 2))
-            loss = classify_loss + LAMBDA * regular_loss
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step() # apply gradient
-            loss_lst.append(loss.item()) # loss recoder
+                # #########################################
+                # #########################################
+                output = logistic_model(DAO.xArray)
+                classify_loss = criterion(output, DAO.yArray)  # compute loss for every net
+                regular_loss = 0
+                for par in logistic_model.parameters():
+                    regular_loss += torch.sum(torch.pow(par, 2))
+                loss = classify_loss + LAMBDA * regular_loss
+                per_iter_loss_lst.append(loss.item())
         print('Epoch [{}/{}], Loss: {:.4f}'.format(num_epoch + 1, EPOCH, loss.item()))
+        # logistic_model.to(device)
     torch.save(logistic_model, "SGD_model_100.pt")
-    with open("loss_sgd.pkl", "wb") as f:
-        pickle.dump(loss_lst, f)
+    # with open("loss_sgd.pkl", "wb") as f:
+    #     pickle.dump(loss_lst, f)
+    with open("loss_sgd_iter.pkl", "wb") as f:
+        pickle.dump(per_iter_loss_lst, f)
+
     print(loss)
 
 
--- src/mini_batch.py
+++ src/mini_batch.py
@@ -1,16 +1,19 @@
 import torch
 from torch import nn
+from torch.autograd import Variable
 from sklearn.datasets import fetch_rcv1
 import numpy as np
 import torch.utils.data as Data
 import pickle
 
-# 超参数
+# 超参数, BATCH_SIZE = 1即SGD
 BATCH_SIZE = 32
 EPOCH = 100
 LAMBDA = 0.01
 
 # 加载数据
+
+
 class DataAccessObject:
     def __init__(self):
         """
@@ -25,20 +28,43 @@ class DataAccessObject:
         rcv1 = fetch_rcv1(subset='train', download_if_missing=False)
         x = rcv1.data.A  # numpy.float64
         x = x.astype(np.float32)  # 修改数据类型，否则就会出错
-        self.xArray = torch.from_numpy(x)
+        self.xArray = torch.from_numpy(x)[:23040]
         # csr_matrix -> numpy.ndarray -> torch.tensor
         y = rcv1.target.A
         y = y.astype(np.float32)  # 修改数据类型，否则就会出错
-        self.yArray = torch.from_numpy(y)
+        self.yArray = torch.from_numpy(y)[:23040]
 
     def load_batch(self):
         self.torch_dataset = Data.TensorDataset(self.xArray, self.yArray)
         self.loader = Data.DataLoader(
             dataset=self.torch_dataset,
-            batch_size=BATCH_SIZE,
+            batch_size=512,
             shuffle=True,  # true表示每个epoch需要洗牌
             num_workers=2,  # 每次训练有两个线程进行的
         )
+
+
+class data_prefetcher():
+    """ 给dataloader 训练加速"""
+    def __init__(self, loader):
+        self.loader = iter(loader)
+        # self.stream = torch.cuda.Stream()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.next_data = next(self.loader)
+        except StopIteration:
+            self.next_input = None
+            return
+        # with torch.cuda.stream(self.stream):
+        #     self.next_data = self.next_data.cuda(non_blocking=True)
+
+    def next(self):
+        # torch.cuda.current_stream().wait_stream(self.stream)
+        data = self.next_data
+        self.preload()
+        return data
 
 
 # 定义LogisticRegression
@@ -65,38 +91,63 @@ class MyLossFunction(nn.Module):
 
 # optimization process
 if __name__ == '__main__':
+
     logistic_model = LogisticRegression()
     DAO = DataAccessObject()
     criterion = MyLossFunction()
-    use_gpu = torch.cuda.is_available()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
+
     logistic_model.to(device)
     criterion.to(device)
     optimizer = torch.optim.SGD(logistic_model.parameters(),
-                                lr=1e-3)
+                                lr=3)
+    per_iter_loss_lst = []
     loss_lst = []
+    DAO.xArray = DAO.xArray.to(device)
+    DAO.yArray = DAO.yArray.to(device)
 
     for num_epoch in range(EPOCH):
         print(num_epoch)
         for step, (batch_x, batch_y) in enumerate(DAO.loader):
-            # print("before", batch_x.device)
-            batch_x = batch_x.to(device)
-            # print("after", batch_x.device)
-            # print(batch_x.size())
+            batch_x = batch_x.to(device)    # 512
             batch_y = batch_y.to(device)
-            output = logistic_model(batch_x)  # get_out for every net
-            classify_loss = criterion(output, batch_y)  # compute loss for every net
-            regular_loss = 0
-            for par in logistic_model.parameters():
-                regular_loss += torch.sum(torch.pow(par, 2))
-            loss = regular_loss + LAMBDA * classify_loss
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step() # apply gradient
-            loss_lst.append(loss.item()) # loss recoder
+            for i in range(16):   # 512 / 32
+                output = logistic_model(batch_x[i * 32: (i+1) * 32])  # get_out for every net
+                classify_loss = criterion(output, batch_y[i*32: (i+1) * 32])  # compute loss for every net
+                regular_loss = 0
+                for par in logistic_model.parameters():
+                    regular_loss += torch.sum(torch.pow(par, 2))
+                loss = classify_loss + LAMBDA * regular_loss
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step() # apply gradient
+
+                # ############################################################
+                # ############################################################
+                output = logistic_model(DAO.xArray)
+                classify_loss = criterion(output, DAO.yArray)  # compute loss for every net
+                regular_loss = 0
+                for par in logistic_model.parameters():
+                    regular_loss += torch.sum(torch.pow(par, 2))
+                loss = classify_loss + LAMBDA * regular_loss
+                per_iter_loss_lst.append(loss.item())
+                print('iter [{}], Loss: {:.4f}'.format(i, loss.item()))
+        with open("loss_minibatch_iter.pkl", "wb") as f:
+            pickle.dump(per_iter_loss_lst, f)
+
         print('Epoch [{}/{}], Loss: {:.4f}'.format(num_epoch + 1, EPOCH, loss.item()))
-    torch.save(logistic_model, "mini_batchGD_model_epoch1000.pt")
-    with open("loss_minibatch.pkl", "wb") as f:
-        pickle.dump(loss_lst, f)
+        # logistic_model.to(device)
+    torch.save(logistic_model, "minibatch_model_100.pt")
+    with open("loss_minibatch_iter.pkl", "wb") as f:
+        pickle.dump(per_iter_loss_lst, f)
+
     print(loss)
+
+
+
+
+
+
 
